81f7b804f763808930ecd5ee4dba3d87f23cd5d3
mansuur Abdullahi Abdirahman <mansuurtech101@gmail.com>
2023-01-31 15:18:35 +0300

    Fixed nav activation

--- configuration/urls.py
+++ configuration/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     # path('admin/', admin.site.urls),
     path('',  include('Users.users_urls')),
+    path('finance/',  include('Finance.finance_urls')),
 ]
 
 
