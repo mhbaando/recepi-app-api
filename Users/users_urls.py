34ad2e89c19be8331705d4dec8e7d1f566f81c64
mansuur Abdullahi Abdirahman <mansuurtech101@gmail.com>
2023-02-04 10:26:37 +0300

    Started adding users

--- Users/users_urls.py
+++ Users/users_urls.py
@@ -3,5 +3,14 @@ from .views import *
 
 
 urlpatterns = [
+    # Users
+    path('add_users', Users, name='Users'),
+    path('user_lists', UsersList, name='UsersList'),
+    path('customers_list', CustomersList, name='CustomersList'),
+    path('manage_users/<int:id>',
+         ManageUsers, name='ManageUsers'),
+
+
+
     path('', Dashboard, name='Dashboard'),
 ]
