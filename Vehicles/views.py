f8251ce1dfaf741f4af8033fe1964b863c50ca36
Aziza <azizirage012@gmail.com>
2023-02-06 07:39:31 +0300

    changed register

--- Vehicles/views.py
+++ Vehicles/views.py
@@ -2,12 +2,12 @@ from django.shortcuts import render,redirect
 from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
-from .models import vehicle
+from .models import vehicle,color,cylinder,model_brand
 from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy
 from .forms import Vehicleform
 from django.contrib import messages
-
+from Customers.models import customer,countries
 # Create your views here.
 
 
@@ -15,16 +15,34 @@ from django.contrib import messages
 
 
 def register_vehicle(request):
-    form = Vehicleform()
-    if request.method == "POST":
-        form = Vehicleform(request.POST)
-        if form.is_valid():
-            form.save()
-            messages.success(request, "a Vehicle was registerd")
-            return redirect("veiw-vehicle")
-        else:
+    vehicle_model1=model_brand.objects.all()
+    color1=color.objects.all()
+    origin1=countries.objects.all()
+    cylinder1=cylinder.objects.all()
+    owner1=customer.objects.all()
+
+    if request.method=="POST":
+        a=request.post['vehicle_model']
+        b=request.POST['year']
+        c=request.POST['origin']
+        d=request.POST['color']
+        e=request.POST['cylinder']
+        f=request.POST['hp']
+        g=request.POST['pessenger_seat']
+        h=request.POST['vin']
+        i=request.POST['enginer_no']
+        j=request.POST['rv_number']
+        k=request.POST['owner']
+        vehicles=model_brand.objects.filter(name=a).first()
+        colors=color.objects.filter(name=b).first()
+        origin=countries.objects.filter(name=c).first()
+        cylenders=cylinder.objects.filter(name=d).first()
+        owner=customer.objects.filter(name=e).first()
+        vehicle.objects.create(vehicles=vehicles,year=b,origin=c,colors=colors,cylinders=e,hp=f,pessenger_seat=g,vin=h,enginer_no=i,rv_number=j,owner=k)
+    
+    else:
             messages.error(request, "error accured")
-    context = {"form": form}
+    context = {"vehicles":vehicles,"colors":color1,"origin":origin1,"cylinders":cylinder1,"owner":owner1}
     return render(request, "Vehicles/register_vehicle.html", context)
 
 
