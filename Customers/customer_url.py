607f080bad54e40b8dcfdaf44aa2db607563c3ad
halimaHussein <hanahussein6155.com>
2023-02-07 09:03:08 +0300

    updated viw company

--- Customers/customer_url.py
+++ Customers/customer_url.py
@@ -5,9 +5,12 @@ urlpatterns = [
     path('register/', views.register_company, name='register_company'),
 
     path('company-views/', views.view_company, name='view_company'),
-    path('company_detail/', views.detail_company, name='detail_company'),
+
     path('company_block/', views.block_company, name='comapny_block'),
-    path('company_profile/', views.profile, name='comapny_profile'),
+    path('company/profile/<int:id>', views.company_profile, name="company_profile"),
+
+
+
 
 
     # License Urls
