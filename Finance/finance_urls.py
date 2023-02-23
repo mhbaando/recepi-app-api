82fc79c9d05df5acd005eb75abd14b0704cca032
halimaHussein <hanahussein6155.com>
2023-02-23 16:43:10 +0300

    worked on payments

--- Finance/finance_urls.py
+++ Finance/finance_urls.py
@@ -7,9 +7,15 @@ urlpatterns = [
     path('accounts', views.AccountsPage, name='AccountsPage'),
     path('add_account', views.AddAccount, name='AddAccount'),
     path('manage_accounts/<str:action>', views.ManageAccounts),
-    
-    
+
+
     # Receipt End Points
     path('receipt', views.ReceiptPage, name='ReceiptsPage'),
     path('add_receipt', views.AddReceipt, name='AddReceipt'),
+    path('receipt/<int:id>', views.receipt, name='receipt'),
+    path('receipt_voucher/<str:search>',
+         views.SearchReceiptVoucher, name='SearchReceiptVoucher'),
+
+
+
 ]
