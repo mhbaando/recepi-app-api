8946aafce34962b8a0f15b361449bad7d0185482
mhbaando <maxamuudyare21@gmail.com>
2023-03-05 09:14:07 +0300

    updaded git ignore

--- configuration/settings.py
+++ configuration/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         },
         'NAME': config('DATABASE_NAME'),
         'USER': config('DATABASE_USER'),
-        'PASSWORD': '',
+        'PASSWORD': config('DATABASE_PASSWORD'),
         'HOST': config('DATABASE_HOST'),
         'PORT': config('DATABASE_PORT'),
     }
