ca2036ec161a32bf3d339080dca8aaab7a74421b
mhbaando <maxamuudyare21@gmail.com>
2023-03-08 14:19:29 +0300

    recommentd the password

--- configuration/settings.py
+++ configuration/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         },
         "NAME": config("DATABASE_NAME"),
         "USER": config("DATABASE_USER"),
-        # "PASSWORD": config("DATABASE_PASSWORD"),
+        "PASSWORD": config("DATABASE_PASSWORD"),
         "HOST": config("DATABASE_HOST"),
         "PORT": config("DATABASE_PORT"),
     }
