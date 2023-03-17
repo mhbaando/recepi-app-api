961ef2945ef42487634962db17e2b32f6370564a
HalimaHAhmed <hanahussein6155@gmail.com>
2023-03-17 23:00:01 +0300

    reformatted

--- static/scripts/finance/receipt.js
+++ static/scripts/finance/receipt.js
@@ -1,7 +1,6 @@
 $(document).ready(function () {
 
   let rvid = $("#rvnumber").val();
- 
   let persid = $("personal_id").val();
   let rvamount = $("#rv_amount").val();
   let mothername = $("#mother_name").val();
@@ -16,11 +15,11 @@ $(document).ready(function () {
       async: false,
       headers: { "X-CSRFToken": csrftoken },
       success: function (data) {
-        data.Message?.forEach((rfromname ) => {
-          list.push(rfromname );
+        data.Message?.forEach((rfromname) => {
+          list.push(rfromname);
         });
       },
-      
+
     });
     return list;
   }
@@ -38,28 +37,27 @@ $(document).ready(function () {
         data.Message?.forEach((account) => {
           list.push(account);
         });
-        console.log(data)
       },
     });
     return list;
   }
 
- // Performing autocomplete function of recieved from
-  $("#rcfrom").on("input",function(){
+  // Performing autocomplete function of recieved from
+  $("#rcfrom").on("input", function () {
 
     let rfromname = [];
 
-    if($(this).val() != "" && $(this).val().length > 3){
+    if ($(this).val() != "" && $(this).val().length > 3) {
       rfromname = rcfrom($(this).val());
       $("#rcfrom").autocomplete({
-        source:rfromname,
-        select:function(event,ui){
+        source: rfromname,
+        select: function (event, ui) {
           const item = ui.item.value;
           const value = ui.item.value;
           if (value != "") {
             $("#rcfrom").attr("rcfrom", item)
-            $("#personal_id").attr('value',ui.item.per_id )
-            $("#mother_name").attr('value',ui.item.mot_name)
+            $("#personal_id").attr('value', ui.item.per_id)
+            $("#mother_name").attr('value', ui.item.mot_name)
 
           }
         },
@@ -75,7 +73,7 @@ $(document).ready(function () {
   });
 
 
-        
+
 
   // Performing autocomplete function of rv account
   $("#rvaccount").on("input", function () {
@@ -90,8 +88,8 @@ $(document).ready(function () {
           const value = ui.item.rv_account;
           if (value != "") {
             $("#rvaccount").attr("rv_account", item)
-            
-            $("#rv_amount").attr('value','$ '+ui.item.amount )
+
+            $("#rv_amount").attr('value', '$ ' + ui.item.amount)
           }
         },
         response: function (event, ui) {
@@ -104,90 +102,6 @@ $(document).ready(function () {
       });
     }
   });
-
-
-
-
-
-  // finding recived from
-
- 
-
-
-
-
-
-
-
-  // $("#reciet_form").on("submit", function (e) {
-  //   e.preventDefault()
-  //   let formData = new FormData();
-
-
-  //   // Validations
-  //   if (rv_id == undefined || rv_number == "") {
-  //     Swal.fire("Warning!!", "Please enter receipt voucher", "warning");
-  //   }
-
-
-  //   else {
-  //     formData.append("rv_id", rv_id);
-  //     formData.append("rv_number", rv_number);
-  //     formData.append("rvnumber", rvid);
-  //     formData.append("search1", rcfrom);
-  //     formData.append("search1", rvaccount);
-  //     formData.append("personal_id", persid)
-  //     formData.append("rv_amount", rvamount);
-  //     formData.append("mother_name", mothername)
-
-
-
-
-  //     Swal.fire({
-  //       title: "Are you sure",
-  //       text: "to register this recipt ?",
-  //       icon: "warning",
-  //       showCancelButton: !0,
-  //       confirmButtonColor: "#2ab57d",
-  //       cancelButtonColor: "#fd625e",
-  //       confirmButtonText: "Yes, register it!",
-  //     }).then(function (e) {
-  //       if (e.value) {
-  //         $.ajax({
-  //           method: "POST",
-  //           url: "/customer/manage_license/" + 0,
-  //           headers: { "X-CSRFToken": csrftoken },
-  //           processData: false,
-  //           contentType: false,
-  //           data: formData,
-  //           async: true,
-  //           success: function (data) {
-  //             if (!data.isError) {
-  //               Swal.fire({
-  //                 title: data.title,
-  //                 text: data.Message,
-  //                 icon: data.type,
-  //                 confirmButtonColor: "#2ab57d",
-  //                 cancelButtonColor: "#fd625e",
-  //                 confirmButtonText: "Ok it!",
-  //               }).then(function (e) {
-  //                 if (e.value) {
-  //                   window.location.reload();
-  //                 }
-  //               });
-  //             } else {
-  //               Swal.fire(data.title, data.Message, data.type);
-  //             }
-  //           },
-  //           error: function (error) {
-  //             //(error);
-  //           },
-  //         });
-  //       }
-  //     });
-
-  //   }
-  // });
 });
 
 
