b87ce812d5df613f481b57ad0fe002f5cb6ac452
mhbaando <maxamuudyare21@gmail.com>
2023-03-18 14:24:04 +0300

    registered new path for priniting customer

--- configuration/urls.py
+++ configuration/urls.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from Users.views import Login, Logout
 from .views import unauthorized_view
+from . import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',  include('Users.users_urls')),
@@ -12,8 +13,9 @@ urlpatterns = [
     path('finance/',  include('Finance.finance_urls')),
     path('customer/',  include('Customers.customer_url')),
     path('vehicles/',  include('Vehicles.vehicle_urls')),
-    path('anauthorized', unauthorized_view,  name="un_authorized")
-]
+    path('anauthorized', unauthorized_view,  name="un_authorized"),
+    path('print/', include('pdf_converter.urls'))
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
 if settings.DEBUG:
