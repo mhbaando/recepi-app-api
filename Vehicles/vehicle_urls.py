c7a76da1298512914d53f09f8e2ac56d2e944d94
mhbaando <maxamuudyare21@gmail.com>
2023-03-19 14:08:21 +0300

    add new paths

--- Vehicles/vehicle_urls.py
+++ Vehicles/vehicle_urls.py
@@ -37,10 +37,9 @@ urlpatterns = [
     path('reports/vehicle-report/<str:id>', reports.vehicle_report),
 
     # Mot URLs
-    path('regsiter_mots/', register_mot, name="register_mot"),
-    path('manage-mot/', manage_mot, name="manage_mot"),
     path('find_test_el/<int:id>', find_test_el),
     path('find_car/<str:search>', Searchvehicle),
-    path("mot-list/", view_mot, name="view-mot"),
-    path('find_test/<int:id>/', find_edit_test),
+    path('regsiter_mots/', register_mot, name="register_mot"),
+    path('view_mots', view_mot, name='view_mot')
+
 ]
