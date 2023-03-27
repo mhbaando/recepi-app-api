a34fcef017c725b7326a8b7de7bad2c23aebfbee
aziza <azizirage012@gmail.com>
2023-03-27 15:20:54 -0230

    refactored the code

--- Customers/forms.py
+++ Customers/forms.py
@@ -235,25 +235,6 @@ class company_edit(forms.Form):
         return cleaned_data
 
 
-
-
-class reciept_edit(forms.Form):
-
-    amount = forms.CharField(max_length=20, strip=True)
-    reason = forms.CharField(max_length=20, strip=True)
-
-    def clean(self):
-        cleaned_data = super().clean()
-        if not self.is_valid():
-            raise forms.ValidationError('Form is not valid.')
-        for field in self.fields:
-            value = cleaned_data.get(field)
-            if value:
-                cleaned_data[field] = escape(
-                    value)
-        return cleaned_data
-
-
 class Lisence_form(forms.Form):
     federal_state = forms.IntegerField()
     place_issue = forms.IntegerField()
