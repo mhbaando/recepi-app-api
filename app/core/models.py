c359a6252b243d24808a62973864a11b085a3179
mhbaando <maxamuudyare21@gmail.com>
2023-04-02 14:34:27 +0300

    added test and implemnetaton for creating user and super admin

--- app/core/models.py
+++ app/core/models.py
@@ -2,7 +2,7 @@
 Database Models.
 """
 from django.db import models
-from django.contrib.auth.models import(
+from django.contrib.auth.models import (
     AbstractBaseUser,
     BaseUserManager,
     PermissionsMixin
@@ -12,20 +12,33 @@ from django.contrib.auth.models import(
 class UserManager(BaseUserManager):
     """Manager For Users"""
     # **extra_feilds -> we can provide keyword arguments
-    def create_user(self,email, password=None, **extra_feilds):
+    def create_user(self, email, password=None,  **extra_feilds):
         """Create, Save & Return new user"""
-        user = self.model(email=email, **extra_feilds)
+        if not email:
+            raise ValueError('Email is Required')
+        user = self.model(email=self.normalize_email(email), **extra_feilds)
         user.set_password(password)
         user.save(using=self._db)  # to support adding mult db
 
         return user
 
+    # make sure you dont have type inthe name of this fn
+    def create_superuser(self, email, password):
+        """Create and return a new superuser."""
+        user = self.create_user(self.normalize_email(email), password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
 
 # AbstractBaseUser contains functionality for the auth system no any feild
-# PermissionsMixin contains fucntionlity for permission & any fields ineed for permission feature
+# PermissionsMixin contains fucntionlity for
+# permission & any fields ineed for permission feature
 
 
-class User(AbstractBaseUser,PermissionsMixin):
+class User(AbstractBaseUser, PermissionsMixin):
     """User in the system"""
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
@@ -33,7 +46,7 @@ class User(AbstractBaseUser,PermissionsMixin):
     is_staff = models.BooleanField(default=False)
 
     # assign UserManager to our custom model class
-    objects  = UserManager()  # creates instance of the manger
+    objects = UserManager()  # creates instance of the manger
     USERNAME_FIELD = 'email'
 
 # Create user model manager
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -3,7 +3,9 @@ Test for models
 """
 
 from django.test import TestCase
-from django.contrib.auth import get_user_model  # helper fn in order to get the default user model
+from django.contrib.auth import get_user_model
+# helper fn in order to get the default user model
+
 
 class ModelTest(TestCase):
     """Tes Mdels."""
@@ -20,3 +22,32 @@ class ModelTest(TestCase):
         # check if the cretad user & email are equal
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test Email is normalized for the new user"""
+        sample_emails = [
+            ['test1@EXAMPLE.com', 'test1@example.com'],
+            ['Test2@Example.com', 'Test2@example.com'],
+            ['TEST3@EXAMPLE.COM', 'TEST3@example.com'],
+            ['test4@example.COM','test4@example.com']
+        ]
+
+        for email,expected in sample_emails:
+            user = get_user_model().objects.create_user(email, 'sample123')
+            self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raises_error(self):
+        """Test That createting user without email raises a valueError."""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('','sample123')
+
+    def test_create_superuser(self):
+        """Test creating a superuser."""
+        user = get_user_model().objects.create_superuser(
+            'test@example.com',
+            'test123',
+        )
+
+        # is_super user provided by permision mixin
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
