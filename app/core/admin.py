c4506609edb3c447ac20cd5a5c343915b1d1a897
mhbaando <maxamuudyare21@gmail.com>
2023-04-02 15:23:12 +0300

    add test for add user and edit user page

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,49 @@
-from django.contrib import admin # noqa
+"""
+Django Admin Customization
+"""
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
+from core import models
 
-# Register your models here.
+
+class UserAdmin(BaseUserAdmin):
+    """Defin the admin pages for users"""
+    ordering=['id']
+    list_display=['email','name']
+    fieldsets=(
+        ('Basic Info',{'fields':('email','password','name')}),
+        (
+            _('Permissions'),{
+                'fields':(
+                    'is_active',
+                    'is_staff',
+                    'is_superuser'
+                )
+            }
+        ),
+        (_('Important dates'),{
+            'fields':('last_login',)
+        }),
+    )
+    readonly_fields = ['last_login']
+
+    # for add user
+    add_fieldsets = (
+        (None,{
+        'classes':('wide',),
+        'fields':(
+        'email',
+        'password1',
+        'password2',
+        'name',
+        'is_active',
+        'is_staff',
+        'is_superuser'
+        )
+        }),
+    )
+
+# when you register you tell by pass
+# 2nd argument to user the above custom
+admin.site.register(models.User, UserAdmin)
---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,49 @@
+"""
+Tests for the django admin modifications
+"""
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+
+class AdminSiteTest(TestCase):
+    """Test for django admin"""
+
+    # setup methods
+    # run before every setup
+    def setUp(self):
+        """Create user & Client"""
+        self.client = Client()  # for https request
+        self.admin_user = get_user_model().objects.create_superuser(email='admin@example.com',password='testpass123')
+
+        # every req will be authenticateing
+        self.client.force_login(self.admin_user)
+
+        self.user = get_user_model().objects.create_user(
+            email='user@example.com',
+            password='testpass123',
+            name='testuser'
+        )
+
+
+    # now the test
+    def test_users_list(self):
+        """Test That users are listed"""
+        url = reverse('admin:core_user_changelist')
+        res = self.client.get(url)
+
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
+
+    def test_edit_user_page(self):
+        """Test the edit user page works"""
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+        self.assertEqual(res.status_code, 200)
+
+
+    def test_add_user_page(self):
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code,200)
